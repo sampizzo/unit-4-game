1c1e24cc20edc9c5f236efa28f4469c3f22c9415
SP <sampizzo@gmail.com>
2019-10-22 23:27:49 -0400

    added divs with ids to index.html and pseudocode to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//generate random number between 19-120
+//assign random number to var targetScore
+var targetScore = Math.floor(Math.random() * 120) + 19;
+console.log("targetScore: " + targetScore);
+
+
+//display targetScore
+
+//create var playerScore
+
+//create a loop that generates a random number between 1-12 and assign each random number to each of the 4 crystals
+
+//clicking a crystal adds its hidden value to the playerScore
+
+//if playerScore === targetScore, the player wins, increment wins, reset game
+
+//else if playerScore > targetScore, the player loses, increment losses, reset game
+
+//else game continues 
