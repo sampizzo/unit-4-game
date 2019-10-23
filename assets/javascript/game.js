7478932e78b8a7f8ed768ec0e65d103d0bd162a4
SP <sampizzo@gmail.com>
2019-10-22 23:44:28 -0400

    display wins, losses, playerScore, and targetScore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,22 @@
+var wins = 0;
+var losses = 0;
+var playerScore = 0;
+
 //generate random number between 19-120
 //assign random number to var targetScore
 var targetScore = Math.floor(Math.random() * 120) + 19;
 console.log("targetScore: " + targetScore);
 
-
+$("#wins").html("Wins: " + wins);
+$("#losses").html("Losses: " + losses);
 //display targetScore
+$("#target-score").html("Target Score: " + targetScore);
+$("#player-score").html("Player Score: " + playerScore);
 
-//create var playerScore
 
 //create a loop that generates a random number between 1-12 and assign each random number to each of the 4 crystals
+
+
 
 //clicking a crystal adds its hidden value to the playerScore
 
